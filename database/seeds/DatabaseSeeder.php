89d642f5d9794a21d864d6a142856b613fe77a06
najibu <ns.najibu@gmail.com>
2017-06-14 20:35:34 +0100

    Created tags and lesson tag pivot table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Tag;
 use App\Lesson;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    private $tables = [
+        'lessons',
+        'tags'
+    ];
     /**
      * Run the database seeds.
      *
@@ -12,9 +17,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Lesson::truncate();
+        
+        $this->cleanDatabase();
 
         factory(Lesson::class, 30)->create();
+        $this->call(TagsTableSeeder::class); 
         
+    }
+
+    public function cleanDatabase()
+    {
+        // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Schema::disableForeignKeyConstraints();
+
+        foreach ($this->tables as $tableName) {
+            DB::table($tableName)->truncate();
+        }
+
+        Schema::enableForeignKeyConstraints();
+
+        // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
     }
 }
